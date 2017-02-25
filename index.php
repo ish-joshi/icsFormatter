b2a17bdc934666b967a1be634455f78c894992e9
Ishan Joshi <ijos0001@student.monash.edu>
2017-02-25 14:13:49 +1100

    BUG FIX: now includes the last week in the .ics file which was previously omitted

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ishan
+ * Date: 25/02/2017
+ * Time: 1:01 PM
+ */
+
+///This is a new version of the website that converts Allocate+ to .ics/.ical files.
+?>
+
+<!DOCTYPE html>
+<head>
+    <title>Timetabld - Share, Connect and Optimise</title>
+
+</head>
